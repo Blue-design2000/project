0fb82da9a1bea1101b21099a83391338568f1c3a
Propriétaire <alexandre.khalfallah@efpedu.fr>
2021-10-04 22:34:23 +0200

    requetes back fini

--- entreprise/pages/Commandes.js
+++ entreprise/pages/Commandes.js
@@ -1,20 +1,39 @@
 import React from 'react';
 import styles from '../../styles'
 import { Text, View } from 'react-native';
-import {ListItem} from 'react-native-elements';
+//import {ListItem} from 'react-native-elements';
 import axios from 'axios';
-var ws = new WebSocket('ws://localhost:3000');
-const list=[[],[],[]]
+const Commandes=()=> {
+const [uid,setUid]=React.useState("")
+const [commandes, setCommandes]=React.useState([[],[],[]])
+  var ws = new WebSocket('ws://localhost:3000');
 ws.onopen = () => {
   // connection opened
   console.log("open")
-  ws.send("something from client"); // send a message
+  ws.send("{\"nature\":\"init\",idrest:\"e@e.fr\"}")// on envoie l'id du restaurant au serveur 
 };
 
 ws.onmessage = (e) => {
-  
+  let data=e.data
+  console.log(data)
   // a message was received
-  console.log(e.message);
+  if(data.nature=="init"){//si on est entrian d'initialiser  
+    setUid(data.uid)// one enrgistre notre uid
+  }else if(data.nature=="add"){// si une nouvelle commande est ajoutee
+    let newCommandes=commandes// on prend les commandes
+    newCommandes[data.etat].append(data.string)// on ajoute la nouvelle commande au tableau correspondant a son etat
+    setCommandes(newCommandes)// on met a jour les commandes
+  }else if(data.nature=="etat"){// si c'est l'etat d'une commande qui change 
+    let newCommandes=commandes// on prend les commandes 
+    newCommandes.map((tab)=>{tab.map((com)=>{
+      if(com.includes("id\":"+data.id)){
+        com.replaceAt(com.indexOf("state\":")+10,data.etat)// on remplace dans la commande correspondant a l'id l'etat (+10 a confirmer c'es tpour tomber pile sur l'etat)
+      }
+      }
+      )
+    })
+    setCommandes(newCommandes);// on met a jour les commandes
+  }
 };
 
 ws.onerror = (e) => {
@@ -26,7 +45,7 @@ ws.onclose = (e) => {
   // connection closed
   console.log(e.code, e.reason);
 };
-const Commandes=()=> {
+
   return (
     <View styles={styles.container}>
       <Text>Commandes</Text>
@@ -57,6 +76,7 @@ const Commandes=()=> {
       </ListItem>
     ))
     }
+  
     </View>
 )};
 export default Commandes; 
--- serveur/App.js
+++ serveur/App.js
@@ -12,12 +12,41 @@ const sql = require("./database.js");
 const http = require('http');
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-
+let id={}
+function uniqueID() {
+  return Math.floor(Math.random() * Date.now())
+  }
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {  
-    console.log(JSON.stringify(message))// J ARRIVE PAS A AVOIR ACCES AUX DONNEES 
+  ws.on('message', function incoming(message) {
+    let clairMessage= message.toString()
+    console.log(clairMessage)
+    if(clairMessage.nature=="init"){// si c'est le message de connexion on enregistre le restaurant 
+      id[clairMessage.uid].append(clairmessage.idrest)
+    }else if(clairMessage.nature=="etat"){// si il envoie pour changer l'etat on envoie a tous les gens du restau et on change dans la BDD 
+      let filtered = Object.fromEntries(Object.entries(id).filter(([k,v]) => v[1]==clairMessage.idrest));// on prend tous les gens qui ont le meme id que l'envoyeur
+      Object.keys(filtered).forEach((x)=>id[x][0].send(JSON.toString({nature:"etat",id: clairMessage.id,etat:changeState[+1==clairMessage.etat]+1})))// on envoie le changement a tout le monde
+      let changeState=["+","-"]
+      sql.query("UPDATE commandes SET state=state"+changeState[+1==clairMessage.etat]+"1 WHERE idrest='"+clairMessage.idrest+"'",(err, res) => {
+        if (err) {
+        console.log("error: ", err);
+        res.send(err);
+        return;
+      }})
+    }else if (clairMessage.nature=="add"){// si c'est pour enregistrer un nouveau menu on envoie a tout le monde et dans la bdd 
+      let filtered = Object.fromEntries(Object.entries(id).filter(([k,v]) => v[1]==clairMessage.idrest));
+      Object.keys(filtered).forEach((x)=>id[x][0].send(JSON.toString({nature:"add",string:clairMessage.string})))
+      sql.query("INSERT INTO commandes SET ?",clairMessage.data,(err, res) => {
+        if (err) {
+        console.log("error: ", err);
+        res.send(err);
+        return;
+      }})
+    }
+    console.log(message.toString())// J ARRIVE PAS A AVOIR ACCES AUX DONNEES 
   });
-  ws.send('something from server');
+  let uid=uniqueID()
+  id[uid]=[ws]
+  ws.send("{\"nature\":\"init\",\"id\":"+uid+"}");
 });
 server.listen(port, function(err) {
   if (err) {
